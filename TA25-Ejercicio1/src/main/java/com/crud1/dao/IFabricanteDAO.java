7c4faf137cac514192f2c487eb976287d73d3ca2
DanielFloria <59457076+DanielFloria@users.noreply.github.com>
2022-08-17 12:29:11 +0200

    Delete TA25-Ejercicio1 directory

--- TA25-Ejercicio1/src/main/java/com/crud1/dao/IFabricanteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.crud1.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud1.dto.Fabricante;
-
-public interface IFabricanteDAO extends JpaRepository<Fabricante, Long>{
-
-}
